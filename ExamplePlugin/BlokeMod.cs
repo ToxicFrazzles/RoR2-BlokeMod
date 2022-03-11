e9dc93387484fd7ba1aff984b2deb056ea3467c3
Frazer Doyle <admin@blokegaming.com>
2022-03-11 23:09:52 +0000

    Initial code commit

---
+++ ExamplePlugin/BlokeMod.cs
@@ -0,0 +1,78 @@
+using BepInEx;
+using RoR2;
+using RoR2.Stats;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExamplePlugin
+{
+	//This is an example plugin that can be put in BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
+    //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
+
+	
+	//This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+	
+	
+	//This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class BlokeMod : BaseUnityPlugin
+	{
+        //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
+        //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "ToxicFrazzles";
+        public const string PluginName = "BlokeMod";
+        public const string PluginVersion = "1.0.0";
+
+		//The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            On.RoR2.PurchaseInteraction.CanBeAffordedByInteractor += (orig, self, activator) =>
+            {
+                return self.costType == CostTypeIndex.LunarCoin || orig(self, activator);
+            };
+
+            On.RoR2.PurchaseInteraction.OnInteractionBegin += (orig, self, activator) =>
+            {
+                if(self.costType == CostTypeIndex.LunarCoin)
+                {
+                    self.onPurchase.Invoke(activator);
+                    self.lastActivator = activator;
+                }
+                else
+                {
+                    orig(self, activator);
+                }
+            };
+
+            On.RoR2.BarrelInteraction.OnInteractionBegin += (orig, self, activator) =>
+            {
+                orig(self, activator);
+                Destroy(self.gameObject);
+            };
+
+            On.RoR2.ChestBehavior.ItemDrop += (orig, self) =>
+            {
+                orig(self);
+                Destroy(self.gameObject);
+            };
+        }
+
+        //The Update() method is run on every frame of the game.
+        private void Update()
+        {
+            //This if statement checks if the player has currently pressed F2.
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+
+                //And then drop our defined item in front of the player.
+
+                Log.LogInfo($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+            }   
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RoR2-BlokeMod
 Miscellaneous Risk of Rain 2 mod
+
+Currently does the following:
+* Delete opened chests and barrels
+* Removes lunar coin cost
