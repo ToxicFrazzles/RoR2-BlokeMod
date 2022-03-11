1758d0be35cb6e549d64cd3c3508416450c511bc
Frazer Doyle <admin@blokegaming.com>
2022-03-11 23:17:37 +0000

    Remove useless parts and renamed subdirectory

--- BlokeMod/BlokeMod.cs
+++ BlokeMod/BlokeMod.cs
@@ -60,19 +60,17 @@ namespace ExamplePlugin
         }
 
         //The Update() method is run on every frame of the game.
-        private void Update()
-        {
-            //This if statement checks if the player has currently pressed F2.
-            if (Input.GetKeyDown(KeyCode.F2))
-            {
-                //Get the player body to use a position:	
-                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-
-                //And then drop our defined item in front of the player.
-
-                Log.LogInfo($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
-                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
-            }   
-        }
+        //private void Update()
+        //{
+        //    //This if statement checks if the player has currently pressed F2.
+        //    if (Input.GetKeyDown(KeyCode.F2))
+        //    {
+        //        //Get the player body to use a position:	
+        //        var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+        //
+        //        //And then drop our defined item in front of the player.
+        //        //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+        //    }   
+        //}
     }
 }
